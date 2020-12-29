cd48fb7d427fd3a09ee55774f2901b02f72d5ccb
Joshua Avalon <git@avalon.sh>
2020-12-29 22:57:38 +0800

    feat: change image from s3

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
-import { APIGatewayEvent, Context } from 'aws-lambda'
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import sharp from "sharp";
+import { Client } from "minio";
 
-export async function handler (
-  event: APIGatewayEvent,
-  context: Context
-) {
+const createClient = (): Client => {
+  const {
+    S3_SECRET_KEY: secretKey = "",
+    S3_ENDPOINT: endPoint = "",
+    S3_ACCESS_KEY: accessKey = "",
+    S3_REGION: region = ""
+  } = process.env;
+  return new Client({ endPoint, secretKey, accessKey, region });
+};
 
-  const { msg } = event.queryStringParameters
-
+export const handler: APIGatewayProxyHandlerV2 = async event => {
+  const { key = "", bucket = "" } = event.queryStringParameters || {};
+  const client = createClient();
+  try {
+    client.statObject(bucket, key);
+  } catch (e) {
+    return { statusCode: 404 };
+  }
+  const rs = await client.getObject(bucket, key);
+  const filter = sharp().png();
+  const buffer = await rs.pipe(filter).toBuffer();
   return {
     statusCode: 200,
-     headers: {
-      'Content-Type': 'application/json'
+    headers: {
+      "Content-Type": "image/png"
     },
-    body: JSON.stringify({ msg })
-  }
-}
+    body: buffer.toString("base64"),
+    isBase64Encoded: true
+  };
+};
